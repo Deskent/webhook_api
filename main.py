e52348ff3756aab823d445a2e639f371501e48fd
Deskent <battenetciz@gmail.com>
2022-06-27 15:23:06 +0300

    [build:1.0.3] [version:1.0]
    add DeployData class

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from routers import api_router
 from database import db_connect
 from config import logger
 from services.utils import send_message_to_admins
-from _resources import __version__, __appname__
+from _resources import __version__, __appname__, __build__
 
 
 @logger.catch
 def get_application() -> FastAPI:
     """Start func"""
 
-    send_message_to_admins(f"{__appname__.title()} started. Version: {__version__}")
+    send_message_to_admins(f"{__appname__.title()} started."
+                           f"\nBuild:[{__build__}]"
+                           f"\nVersion:[{__version__}]"
+    )
     application = FastAPI()
     db_connect(application)
     application.include_router(api_router)
