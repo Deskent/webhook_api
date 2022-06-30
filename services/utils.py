36b25c2ff08a459308b39925267ae52de4144165
Deskent <battenetciz@gmail.com>
2022-06-30 21:17:25 +0300

    [build:1.0.6] [version:1.0]
    add tests patterns

--- services/utils.py
+++ services/utils.py
@@ -3,23 +3,29 @@ import requests
 from config import settings, logger
 
 
-def send_message_to_admins(text: str) -> None:
+def send_message_to_admins(text: str) -> int:
     text = '[Slarti]:' + text
     for admin in settings.ADMINS:
         url: str = f"https://api.telegram.org/bot{settings.TELEBOT_TOKEN}/sendMessage?chat_id={admin}&text={text}"
         try:
-            requests.get(url, timeout=5)
+            response = requests.get(url, timeout=5)
+            return response.status_code
         except Exception as err:
             logger.error(f"requests error: {err}")
+    return -1
 
 
-def send_message_to_user(telegram_id: int, message: str) -> None:
+def send_message_to_user(telegram_id: int, message: str) -> int:
     """
     The function of sending the message with the result of the script
     """
     url: str = f"https://api.telegram.org/bot{settings.TELEBOT_TOKEN}/sendMessage?chat_id={telegram_id}&text={message}"
     try:
-        requests.get(url, timeout=5)
+        response = requests.get(url, timeout=5)
+
         logger.info(f"telegram id: {telegram_id}\n message: {message}")
+        return response.status_code
     except Exception as err:
         logger.error(f"telegram id: {telegram_id}\n message: {message}\n requests error: {err}")
+
+    return -1
